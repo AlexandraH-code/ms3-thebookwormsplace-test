31344da4832967bf635f2fa8aa2173383df0a065
AlexandraH-code <alexandra_holstensson@hotmail.com>
2025-05-09 14:50:21 +0200

    Add Post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'published_at')
+    list_filter = ('status', 'created_on', 'published_at')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    created_on = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
